09e4ed82a7213966505beb532fbb5518c61b8112
wchxu96 <929130441@qq.com>
2016-10-24 19:40:45 +0800

    some debug detective

--- GBNserver.py
+++ GBNserver.py
@@ -13,31 +13,35 @@ client --> server
 
 '''
 # data_pkt = namedtuple('data_pkt','seq_num')
-data_pkt = namedtuple('data_pkt','seq_num','data')
-#ack_pkt = namedtuple('ack_pkt','seq_num','yes')
+data_pkt = namedtuple('data_pkt', 'seq_num', 'data')
+# ack_pkt = namedtuple('ack_pkt','seq_num','yes')
 ack_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = socket.gethostbyname()
-port = 2333 # client port
-expected_seq_num = 0 #the number expected to get
+port = 2333  # client port
+expected_seq_num = 0  # the number expected to get
 
 
 def send_ack(seq_num):
     ack_message = [seq_num, 'y']
     ack_socket.sendto(pickle.dumps(ack_message), (host, port))
 
+
 def main():
     global expected_seq_num
     port = int(sys.argv[1])
     out = sys.argv[2]
     prob_loss = sys.argv[3]
-    socketserver = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    socketserver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     socketserver.bind(socket.gethostbyname(), port)
     lost_seq_num = []
     while 1:
-        data,address = socketserver.recvfrom(100000)
+        data, address = socketserver.recvfrom(100000)
         data = pickle.loads(data)
         get_seq_num = data[0]
-        if get_seq_num == expected_seq_num + 1:
+        rand = random.random()
+        if rand <= prob_loss:
+            print 'Packet loss, sequence number' + str(get_seq_num)
+        elif get_seq_num == expected_seq_num + 1:
             send_ack(get_seq_num)
             expected_seq_num += 1
             with open('result.txt', 'ab') as f:
@@ -45,12 +49,5 @@ def main():
         elif get_seq_num > expected_seq_num + 1:
             send_ack(expected_seq_num)
 
-
-
-
 if __name__ == '__main__':
     main()
-
-
-
-
