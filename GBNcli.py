aafd19706c045479d629a93422597ec4f876bab5
wchxu96 <929130441@qq.com>
2016-10-24 20:29:39 +0800

    some debug detective

--- GBNcli.py
+++ GBNcli.py
@@ -1,2 +1,25 @@
 import socket
-import
+from collections import namedtuple
+import pickle
+
+
+data_pkt = namedtuple('data_pkt','seq_num','data')
+MAX_WINDOW_wide = 5
+
+
+def preparetosend(src):
+    allitem = []
+    i = 1
+    with open(src) as f:
+        for str in f:
+            item = data_pkt(seq_num=i,data=str)
+            m = pickle.dumps(item)
+            allitem.append(m)
+            i += 1
+    return allitem
+
+if __name__ == '__main__':
+    cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = socket.gethostbyname()
+    port = 2333
+    cli.bind((host, port))
--- GBNserver.py
+++ GBNserver.py
@@ -17,7 +17,7 @@ data_pkt = namedtuple('data_pkt', 'seq_num', 'data')
 # ack_pkt = namedtuple('ack_pkt','seq_num','yes')
 ack_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = socket.gethostbyname()
-port = 2333  # client port
+port = 2333 # client
 expected_seq_num = 0  # the number expected to get
 
 
@@ -28,9 +28,8 @@ def send_ack(seq_num):
 
 def main():
     global expected_seq_num
-    port = int(sys.argv[1])
-    out = sys.argv[2]
-    prob_loss = sys.argv[3]
+    port = 3377
+    prob_loss = 0.4
     socketserver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     socketserver.bind(socket.gethostbyname(), port)
     lost_seq_num = []
@@ -41,11 +40,13 @@ def main():
         rand = random.random()
         if rand <= prob_loss:
             print 'Packet loss, sequence number' + str(get_seq_num)
+            pass
         elif get_seq_num == expected_seq_num + 1:
             send_ack(get_seq_num)
             expected_seq_num += 1
             with open('result.txt', 'ab') as f:
                 f.write(data[1])
+                f.write('\n')
         elif get_seq_num > expected_seq_num + 1:
             send_ack(expected_seq_num)
 
