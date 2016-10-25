f54cfa3b34f19c8cbd419d86391c7d9bb923513c
wchxu96 <929130441@qq.com>
2016-10-25 15:13:32 +0800

    init

--- GBNcli.py
+++ GBNcli.py
@@ -1,28 +1,80 @@
+# coding=utf-8
 import socket
 from collections import namedtuple
 import pickle
+import time
+import signal
 
-
-data_pkt = namedtuple('data_pkt','seq_num','data')
+srcfile = 'test.txt'
+data_pkt = namedtuple('data_pkt', 'seq_num', 'data')
 MAX_WINDOW_wide = 5
+cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = socket.gethostbyname()
+port = 2333# client port
+cli.bind((host, port))
+serverhost = socket.gethostbyname()
+serverport = 3377
+all_pkt = []  # 所有的数据包状态　bool数组，环形?已发送设置为true,未发送设置为false
+window_low = 0
+window_high = int(MAX_WINDOW_wide) #窗口不包括window_high  --- [window_low ~ window_high - 1]窗口位置
+time_out = 2#超时时间
+allitem = []#　所有数据包
+curitem = 0#当前要发送的数据包
+ackeditem = 0 #已确认的数据包
 
 
 def preparetosend(src):
-    allitem = []
-    i = 1
+    global allitem
+    i = 0
     with open(src) as f:
         for str in f:
-            item = data_pkt(seq_num=i,data=str)
+            item = data_pkt(seq_num=i, data=str)
             m = pickle.dumps(item)
             allitem.append(m)
+            all_pkt.append(False)  # 初始化为全false
             i += 1
-    return allitem
+    return
 
-def time_out:
-    pass
+# 获取已经pickle过的持久化对象列表作为发送的数据包，并根据行数初始化all_pkt数组
+def send_item():
+    global serverport,serverhost
+    global window_low,window_high
+    signal.alarm(0)
+    signal.setitimer(signal.ITIMER_REAL, time_out)
+    for i in range(window_low,window_high):
+        cli.sendto(allitem[i],(serverhost,serverport))
+
+#超时处理函数，此时将获得一个SIG_ALm信号
+def time():#重发
+    '''
+    global window_low, window_high
+    for i in range(window_low,window_high):
+        signal.alarm(0)
+        signal.setitimer(signal.ITIMER_REAL,time_out)
+        cli.sendto(allitem[i],(host,port))
+    '''
+
+
+
+
+def main():
+    preparetosend(srcfile)
+    signal.signal(signal.SIGALRM,send_item)
+    serverhost = socket.gethostbyname()
+    serverport = 3377
+    cli.connect((serverhost,serverport))
+    while ackeditem != len(allitem) - 1:
+        i = window_low
+        send_item()
+
+
+
+
+
+
+def set_timer():
+
+
 
 if __name__ == '__main__':
-    cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    host = socket.gethostbyname()
-    port = 2333
-    cli.bind((host, port))
+    pass
--- GBNserver.py
+++ GBNserver.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import socket
 from collections import namedtuple
 import pickle
@@ -15,23 +16,23 @@ client --> server
 # data_pkt = namedtuple('data_pkt','seq_num')
 data_pkt = namedtuple('data_pkt', 'seq_num', 'data')
 # ack_pkt = namedtuple('ack_pkt','seq_num','yes')
-ack_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host = socket.gethostbyname()
-port = 2333 # client
+port = 3377
+prob_loss = 0.4
+socketserver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socketserver.bind(socket.gethostbyname(), port)
 expected_seq_num = 0  # the number expected to get
 
 
 def send_ack(seq_num):
     ack_message = [seq_num, 'y']
-    ack_socket.sendto(pickle.dumps(ack_message), (host, port))
+    clihost = socket.gethostbyname()
+    cliport = 2333
+    socketserver.sendto(pickle.dumps(ack_message), (clihost, cliport))
 
 
 def main():
     global expected_seq_num
-    port = 3377
-    prob_loss = 0.4
-    socketserver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socketserver.bind(socket.gethostbyname(), port)
+    global socketserver
     lost_seq_num = []
     while 1:
         data, address = socketserver.recvfrom(100000)
@@ -41,14 +42,16 @@ def main():
         if rand <= prob_loss:
             print 'Packet loss, sequence number' + str(get_seq_num)
             pass
-        elif get_seq_num == expected_seq_num + 1:
+        elif get_seq_num == expected_seq_num:
+            print '已按序正确收到'+ str(expected_seq_num)+'数据包'+'\n'
             send_ack(get_seq_num)
             expected_seq_num += 1
             with open('result.txt', 'ab') as f:
                 f.write(data[1])
                 f.write('\n')
-        elif get_seq_num > expected_seq_num + 1:
-            send_ack(expected_seq_num)
+        elif get_seq_num > expected_seq_num:
+            print '已按序正确收到' + str(expected_seq_num - 1) + '数据包' + '\n'
+            send_ack(expected_seq_num-1)
 
 if __name__ == '__main__':
     main()
