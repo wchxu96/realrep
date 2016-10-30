c171809fe1517db88f965b078c46f06775ffb9b2
wchxu96 <929130441@qq.com>
2016-10-30 15:13:06 +0800

    init

---
+++ expressionaddop.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+# leetcode 282 语法分析无头绪,pick one,结果这题巨难感觉，改天再写吧
+'''
+ 282. Expression Add Operators
+
+    Total Accepted: 19182
+    Total Submissions: 69839
+    Difficulty: Hard
+    Contributors: Admin
+
+Given a string that contains only digits 0-9 and a target value, return all possibilities to add binary operators (not unary) +, -, or * between the digits so they evaluate to the target value.
+
+Examples:
+
+"123", 6 -> ["1+2+3", "1*2*3"]
+"232", 8 -> ["2*3+2", "2+3*2"]
+"105", 5 -> ["1*0+5","10-5"]
+"00", 0 -> ["0+0", "0-0", "0*0"]
+"3456237490", 9191 -> []
+
+'''
+
+
+class Solution(object):
+    def addOperators(self, num, target):
+        """
+        :type num: str
+        :type target: int
+        :rtype: List[str]
+        """
+        pass
+
+    def helper(self,num,target,index):
+        pass
--- grammarparser.py
+++ grammarparser.py
@@ -1,28 +1,140 @@
 # coding=utf-8
 import uniout #打印中文用的
+import sys
+sys.setrecursionlimit(100000)
+
+
 class llgrammarparser:
     def __init__(self):
         self.grammar = []
         self.grammarindex = []
+        self.finishsymbol = []
+        self.notfinishsymbol = []#test
+        self.first = {}
+        self.follow = {}
+        self.tonone = []
+        self.index = {}
 
-    def pretodumpleftrec(self):  # 准备文法并消除左递归
+    def pretodumpgrammar(self):#准备文法
+        '''
         #flag = False  # flag为true时一直加入设定为一个推导组
         eachreduce = []
+        finish = []
         with open('grammar.txt') as f:
             for strs in f:
                 if strs.startswith('/*'):
                     self.grammarindex.append(strs.replace('\n',''))
                     self.grammar.append(eachreduce)
+                    self.finishsymbol.append(finish)
                     eachreduce = []
+                    finish = []
+                    notfinish = []
+                elif strs.startswith('@'):
+                    for str in strs:
+                        if str == '@' or str == ' ':
+                            continue
+                        else:
+                            if str == 't': #终结符
+                                tersymbolslice = strs.split(':')
+                                print tersymbolslice
+                                if len(tersymbolslice) > 1:
+                                    for i in map(lambda x: x.replace(' ', ''), tersymbolslice[1].split('|')):
+                                        self.finishsymbol.append(i)
+                                break
+                            elif str == 'n':
+                                ntersymbolslice = strs.split(':')
+                                if len(ntersymbolslice) > 1:
+                                    for i in map(lambda x: x.replace(' ', ''), ntersymbolslice[1].split('|')):
+                                        self.notfinishsymbol.append(i)
+                                break
+                            else:
+                                pass
                 else:
                     strsline = strs.split('->')
                     # k = map(lambda x:x.replace(' ',''),strsline)
                     temp = {}
                     if len(strsline) > 1:
-                        tobereduced = map(lambda x: x.replace('\n',''),strsline[1].split('|'))
+                        tobereduced = map(lambda x: x.replace('\n', ''), strsline[1].split('|'))
                         left = strsline[0]
                         temp[left] = tobereduced
                         eachreduce.append(temp)
-            self.grammar.append(eachreduce)
             self.grammar.pop(0)
-        #消除左递归
+            self.grammar.append(eachreduce)
+            self.finishsymbol.pop(0)
+            #终结符怎么搞?现在看大概只能搞个symboltable进行对比了
+            #
+    #计算first集
+    '''
+        i = 0
+        with open('grammarex.txt') as f:
+            for strs in f:
+                temp = {}
+                dictrightvector = []
+                grammarlist = strs.split('->')
+                if len(grammarlist) > 1:
+                    left = grammarlist[0].strip()
+                    right = grammarlist[1].strip()
+                    rightlist = right.split(' | ')
+                    for item in rightlist:
+                        itemlist = item.split(' ')
+                        dictrightvector.append(itemlist)
+                    temp[left] = dictrightvector
+                    self.grammar.append(temp)
+                    if left not in self.notfinishsymbol:
+                        self.notfinishsymbol.append(left)
+                        self.index[left] = i
+                        i += 1
+                else:
+                    pass
+            for index in self.grammar:
+                values = index.values()
+                for value in values:
+                    for item in value:
+                        for str in item:
+                            if str not in self.notfinishsymbol:
+                                if str not in self.finishsymbol:
+                                    self.finishsymbol.append(str)
+
+    def first(self):
+        # 首先计算终结符的first集合
+        for nter in self.finishsymbol:
+            self.first[nter] = nter #终结符的first集等于其本身
+        for eachrecgroup in self.grammar:
+            #for
+            pass
+
+
+    def iftonone(self,ch): #ch 是否能推导出none
+        if ch in self.tonone:
+            return True
+        elif ch == '$':
+            return True
+        elif ch in self.finishsymbol and ch != '$':
+            return False
+        else:
+            flag = True
+            rank = self.index[ch]
+            for i in self.grammar[rank][ch]:
+                if '$' in i:
+                    self.tonone.append(ch)
+                    return True
+                else:
+                    for item in i:
+                        if not self.iftonone(item):
+                            flag = False
+            return flag
+        #递归爆炸,不仅要判断出ch是否能推导出'$'同时还要将self.tonone列表填充完毕
+
+
+
+
+if __name__ == '__main__':
+    s = llgrammarparser()
+    s.pretodumpgrammar()
+    print s.grammar
+    print s.notfinishsymbol
+    print s.finishsymbol
+    print sorted(s.index.items(), key=lambda d: d[1])
+    print len(s.index)
+    print s.iftonone("E'")
+    #print s.tonone
